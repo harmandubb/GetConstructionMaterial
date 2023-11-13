2d4e1f1820ad00bafbaf2108f7a7978945dfccf7
Harman Dubb <harmand1999@gmail.com>
2023-11-13 00:37:50 -0800

    failed precondition

--- api/google.go
+++ api/google.go
@@ -2,16 +2,28 @@ package api
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/api/gmail/v1"
 )
 
 func connectToGmail() error {
-	_, err := gmail.NewService(context.Background())
-
+	err := godotenv.Load()
 	if err != nil {
 		return err
 	}
+	gmailService, err := gmail.NewService(context.Background())
+	if err != nil {
+		return err
+	}
+
+	messageList, err := gmailService.Users.Messages.List("me").Do()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(messageList)
 
 	return nil
 }
