625b4afe431601918b7f47c6d7bcf58324a41f88
Harman Dubb <harmand1999@gmail.com>
2023-11-26 13:01:43 -0800

    Check is this code compiles correctly else where

--- Front End/getconstructionmaterial/src/app/page.js
+++ Front End/getconstructionmaterial/src/app/page.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import NavBar from '../../components/nav'
+import NavBar from './components/nav'
 
 export default function LandingPage() {
   return (
@@ -13,11 +13,11 @@ export default function LandingPage() {
       <div className="font-mono px-4">
         <h1 className="text-6xl">Looking for Construction Material?</h1>
         <p className="py-4">Get Construction Material will provide you with an easy to search what material is avaialble near you and get the best price possible.</p>
-        <p>Sign up to be alerted when you can start searching!</p>
+        <p>Sign up to be alerted when you can staxrt searching!</p>
       </div>
 
-      <form method="post" class="bg-logo-blue p-4 flex justify-between items-center rounded-lg">
-          <input type="email" placeholder="E-mail Address" class="flex-grow mr-4 p-2 rounded placeholder-gray-500"></input>
+      <form method="post" className="bg-logo-blue p-4 flex justify-between items-center rounded-lg">
+          <input type="email" placeholder="E-mail Address" className="flex-grow mr-4 p-2 rounded placeholder-gray-500"></input>
           <button class="bg-brand-red-dark hover:bg-logo-red-light text-white font-bold py-2 px-4 rounded">
             Stay in the Loop
           </button>
