27fcc58761c0edf1fc06c04a045c047a88f6a9ce
Harman Dubb <harmand1999@gmail.com>
2023-11-30 00:11:22 -0800

    need to link front to sheet

--- Front End/getconstructionmaterial/src/app/components/emailSubmission.js
+++ Front End/getconstructionmaterial/src/app/components/emailSubmission.js
@@ -5,7 +5,7 @@ function EmailSubmission() {
     <form method="post" action="localhost:443/emailForm" className="flex flex-col sm:flex-row sm:w-[600px]">
         <input type="email" placeholder="E-mail Address" className="sm:flex items-stretch flex-grow focus:outline-none block rounded-lg sm:rounded-none sm:rounded-l-lg pl-4 py-2"></input>
         
-        <button className="sm:mt-0 sm:w-auto sm:-ml-2 py-2 px-2 rounded-lg font-medium text-white focus:outline-none bg-logo-blue">
+        <button type="submit" className="sm:mt-0 sm:w-auto sm:-ml-2 py-2 px-2 rounded-lg font-medium text-white focus:outline-none bg-logo-blue">
         Stay in the Loop
         </button>
     </form>
