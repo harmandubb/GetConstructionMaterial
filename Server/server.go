f0d6a9eb96fe077f5b58b1d978f844c1732af192
Harman Dubb <harmand1999@gmail.com>
2023-12-04 22:31:16 -0800

    host added to the port and http

--- Server/server.go
+++ Server/server.go
@@ -139,8 +139,8 @@ func Idle() {
 
 	})
 
-	err := http.ListenAndServeTLS(":8080", getPath("cert.pem"), getPath("key.pem"), nil)
-	// err := http.ListenAndServe(":8080", nil)
+	// err := http.ListenAndServeTLS("0.0.0.0:8080", getPath("cert.pem"), getPath("key.pem"), nil)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
 		log.Fatalf("Sever Error: %v", err)
 	}
