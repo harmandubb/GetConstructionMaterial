1a01505bae5f23946ff04a622d743ba983ef20c2
Harman Dubb <harmand1999@gmail.com>
2023-12-05 23:33:40 -0800

    basic code for health check

--- Server/server.go
+++ Server/server.go
@@ -1,136 +1,24 @@
 package server
 
 import (
-	g "docstruction/getconstructionmaterial/GCalls"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"path/filepath"
-	"runtime"
-	"time"
 )
 
-type EmailFormInfo struct {
-	Time  time.Time
-	Email string
-}
-
-type ServerResponse struct {
-	Success bool
-}
-
-func getPath(relativePath string) string {
-	_, b, _, _ := runtime.Caller(0)
-	// The directory of the file
-	basepath := filepath.Dir(b)
-	// Construct the path relative to the file
-	return filepath.Join(basepath, relativePath)
-}
-
-func setCORS(w http.ResponseWriter, r *http.Request) {
-	// origin := r.Header.Get("Origin")
-
-	// // List of allowed origins
-	// allowedOrigins := map[string]bool{
-	// 	"https://www.docstruction.com":            true,
-	// 	"https://www.getconstructionmaterial.com": true,
-	// }
-
-	// // Check if the origin is in the list of allowed origins
-	// if _, ok := allowedOrigins[origin]; ok {
-	// 	w.Header().Set("Access-Control-Allow-Origin", origin)
-	// }
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-
-}
-
-func Idle() {
-	fmt.Println("Starting Server")
-
-	// TODO: Implement the serveMUX
-
+func idle() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Root Request")
-		setCORS(w, r)
-
-		// Handle OPTIONS for preflight
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/plain")
+		log.Println("Health check request received")
 
 		// Write an HTTP 200 OK status
 		w.WriteHeader(http.StatusOK)
 
-		// Send a response body, which can be a simple message
-		w.Write([]byte("Service is up and running"))
-
+		// Send a response body
+		w.Write([]byte("OK"))
 	})
 
-	http.HandleFunc("/emailForm", func(w http.ResponseWriter, r *http.Request) {
-		setCORS(w, r)
-
-		// Handle OPTIONS for preflight
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		if r.Method == http.MethodPost {
-			w.Header().Set("Content-Type", "application/json")
-			fmt.Println("I am in the email form branch")
-
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusExpectationFailed)
-				return
-			}
-
-			var emailFormInfo EmailFormInfo
-
-			err = json.Unmarshal(body, &emailFormInfo)
-			if err != nil {
-				w.WriteHeader(http.StatusExpectationFailed)
-				return
-			}
-
-			spreadsheetID := "1ZowyzJ008toPYNn0mFc2wG6YTAop9HfnbMPLIM4rRZw" //could make the storing of the id better.
-
-			result := g.SendEmailInfo(emailFormInfo.Time, emailFormInfo.Email, spreadsheetID)
-
-			resp := ServerResponse{
-				Success: result,
-			}
-
-			fmt.Println(resp.Success)
-
-			jsonResp, err := json.Marshal(resp)
-			if err != nil {
-				w.WriteHeader(http.StatusExpectationFailed)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			fmt.Println(jsonResp)
-
-			fmt.Println("Sending response")
-
-			w.Write(jsonResp)
-		}
-
-	})
-
-	// err := http.ListenAndServeTLS("0.0.0.0:8080", getPath("cert.pem"), getPath("key.pem"), nil)
-	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	log.Println("Server is starting on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
-		log.Fatalf("Sever Error: %v", err)
+		log.Fatalf("Server failed to start: %v", err)
 	}
-
 }
