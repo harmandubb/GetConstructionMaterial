cf82fcf47e79b82afdf92cb85616325f80b524c1
Harman Dubb <harmand1999@gmail.com>
2023-12-24 00:09:03 -0800

    Completed all updateCustomer INquiry functions

--- database/customers.go
+++ database/customers.go
@@ -46,7 +46,8 @@ func AddBlankCustomerInquiry(p *pgxpool.Pool, matForm g.MaterialFormInfo, databa
 // sqlString string --> the command that will be sent to the table to read row information
 // args any --> arguements needed to fill and accomplish the readRowInfo
 // Return:
-//
+// custInquiry CustomerInquiry struct --> Retruns what ever infromation is present for the customer in quiry so far
+// Error if present
 
 func readCustomerInquiry(tableName string, customerEmail string) (custInquiry CustomerInquiry, err error) {
 	sqlString := fmt.Sprintf("SELECT * FROM %s WHERE email = '%s'", tableName, customerEmail)
@@ -78,4 +79,58 @@ func readCustomerInquiry(tableName string, customerEmail string) (custInquiry Cu
 
 	return custInquiry, nil
 
+}
+
+// Purpose: update the customer inquiry row given a customer email
+// Parameters:
+// p *pgxpool.Pool --> pointer to the databse connection
+// databse string --> database that you want to connect to
+// tableName string --> table that hods the data that you would like to change
+// customerEmailer string --> email will be used to pull out a row to update
+// col string --> column you want to update
+// val any --> variable to update the col with
+// Return:
+// Error if any present
+
+func updateCustomerInquiry(p *pgxpool.Pool, database string, tableName string, customerEmail string, col string, val any) (err error) {
+	sqlString := fmt.Sprintf("UPDATE %s SET %s = $1 WHERE email = $2", tableName, col)
+
+	err = dataBaseTransmit(p, sqlString, database, val, customerEmail)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Purpose: Speciailized version to update Present of customer inquiry given an email
+func updateCustomerInquiryPresent(p *pgxpool.Pool, database string, tableName string, customerEmail string, present bool) (err error) {
+	err = updateCustomerInquiry(p, database, tableName, customerEmail, "present", present)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Purpose: Speciailized version to update Price and the currency of customer inquiry given an email
+func updateCustomerInquiryPrice(p *pgxpool.Pool, database string, tableName string, customerEmail string, price float64, currency string) (err error) {
+	err = updateCustomerInquiry(p, database, tableName, customerEmail, "price", price)
+	if err != nil {
+		return err
+	}
+
+	err = updateCustomerInquiry(p, database, tableName, customerEmail, "currency", currency)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Purpose: Speciailized version to update DATASHEET of customer inquiry given an emai.
+// ---------TODO------
+
+func updateCustomerInquiryDataSheet(p *pgxpool.Pool, database string, tableName string, customerEmail string, price float64, currency string) (err error) {
+	return nil
 }
