d479cc22efb5f9eadf393d9203e4f6ba9a71079b
Harman <harmand1999@gmail.com>
2024-01-08 20:43:52 -0800

    Added Webpage cononical line for indexing

--- Front End/getconstructionmaterial/src/app/page.tsx
+++ Front End/getconstructionmaterial/src/app/page.tsx
@@ -8,29 +8,40 @@ import ProductSubmissionComponent from './components/productSubmission'
 export default function LandingPage() {
   return (
 
+    <html>
+      <head>
+        <title>Get Construction Material</title>
+        <link rel="canonical" href="https://www.getconstructionmaterial.com"/>
+      </head>
 
-    <body className = "">
-      <NavBar />
+      <body className = "">
+          <NavBar />
 
-      <main className="min-h-screen bg-brand flex flex-col items-center w-full pt-4 px-8">
+          <main className="min-h-screen bg-brand flex flex-col items-center w-full pt-4 px-8">
 
-        <div className="dark:text-black">
-          <div className="font-mono">
-            <h1 className="text-5xl sm:text-6xl pb-4">Looking for Construction Material?</h1>
-            <p className="pb-4">Get Construction Material will provide you with an easy way to search what material is avaialble near you and get the best price possible.</p>
+            <div className="dark:text-black">
+              <div className="font-mono">
+                <h1 className="text-5xl sm:text-6xl pb-4">Looking for Construction Material?</h1>
+                <p className="pb-4">Get Construction Material will provide you with an easy way to search what material is avaialble near you and get the best price possible.</p>
+                
+                <h2 className="pb-4 sm:pt-0 font-bold sm:text-xl">We will provide you with material information within 2 business days.</h2>
+                <p className="sm:pt-0 pb-4">Tell us what you are looking for below and we will try our best to help with your construction needs.</p>
+              </div>
+
+              <ProductSubmissionComponent />
+
+            </div>
             
-            <h2 className="pb-4 sm:pt-0 font-bold sm:text-xl">We will provide you with material information within 2 business days.</h2>
-            <p className="sm:pt-0 pb-4">Tell us what you are looking for below and we will try our best to help with your construction needs.</p>
-          </div>
+          </main>
+          <Analytics />
+          <SpeedInsights />
+        </body>
 
-          <ProductSubmissionComponent />
 
-        </div>
-        
-      </main>
-      <Analytics />
-      <SpeedInsights />
-    </body>
+    </html>
+
+
+    
 
     
     
